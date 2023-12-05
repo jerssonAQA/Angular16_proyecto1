55cb690569313b3177f0eb3a989522c6d7027b59
Jersson Quintero <jersson.9.5@gmail.com>
2023-12-05 17:23:01 -0500

    Eventos propiedades y ngModel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-api1';
+  name = 'Jersson';
+  age = 20;
+  img = "https://source.unsplash.com/random";
+  btnDisabled= true
+  person ={
+    name:'Jersson',
+    age: 25,
+    avatar:"https://source.unsplash.com/random"
+  }
+
+  toogleButton(){
+    this.btnDisabled=!this.btnDisabled
+  }
+  Increment_Age(){
+    this.person.age++;
+  }
+  onScroll(event: Event ){
+    const element = event.target as HTMLElement;
+    console.log(element.scrollTop);
+  }
+  changeName(event: Event){
+    const element = event.target as HTMLInputElement;
+    this.person.name = element.value;
+  }
 }
